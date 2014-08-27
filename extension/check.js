37489e068bb10c2f057f1893b5feda974bfd7bb9
Arsen Mamikonyan <esqaw@riseup.net>
2014-08-27 10:26:20 +0400

    Can't get highlighting to work (seems like js file doesn't load)

--- extension/check.js
+++ extension/check.js
@@ -9,37 +9,27 @@ var applySpellCheck = function(text) {
 }
 
 var highlight_errors = function(textarea) {
-    // Assumes there is a div with 'under-%s' id
-    var styleCss = document.defaultView.getComputedStyle(textarea, "").cssText;
-    var underDiv = document.getElementById('under-' + textarea.id);
-    underDiv.style.cssText = styleCss;
-    underDiv.className = " overlayTextarea";
-    underDiv.style.zIndex = '-1';
-    underDiv.innerHTML = applySpellCheck(textarea.value);
+    var bad_words = [];
+    var words = textarea.value.split(' ');
+    for (var i=0; i<words.length; i++) {
+        word = words[i];
+        if (hygir.isInLanguage(word) && !hygir.isKnown(word)) {
+            bad_words.push(word);
+        }
+    }
+
+    textarea.highlightTextarea({
+        words: bad_words
+    });
 }
 
+
+// Add listeners
 var textareas = document.getElementsByTagName("textarea");
 
 for (var i=0, len=textareas.length; i<len; i++) {
     var textarea = textareas[i];
-    var originalStyle = document.defaultView.getComputedStyle(textarea, "").cssText
-    var outerDiv = document.createElement("div");
-    outerDiv.style.position = "relative";
-    outerDiv.id = 'outer-' + textarea.id;
-    var _parent = textarea.parentElement;
-    _parent.removeChild(textarea);
-    _parent.appendChild(outerDiv);
-    textarea.className += " realTextarea";
-    outerDiv.appendChild(textarea);
-    underDiv = document.createElement("div");
-    underDiv.style.cssText = originalStyle;
-    underDiv.id = 'under-' + textarea.id;
-    underDiv.className = " overlayTextarea";
-    outerDiv.appendChild(underDiv);
-
-    textareas[i].onkeyup = function (evt) {
-        
-        highlight_errors(textarea);
-    }  
+    textarea.onkeyup = function (evt) {
+        highlight_errors(this);
+    };
 }
-
